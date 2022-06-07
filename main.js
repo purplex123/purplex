32b85b23e3cc77892a80d1e8f15eaa21cd91216c
purplex123 <107062628+purplex123@users.noreply.github.com>
2022-06-07 18:47:30 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var countDownDate = new Date("June 22 2022 18:00:00 GMT+0000").getTime();
+
+        var x = setInterval(function () {
+
+            var now = new Date().getTime();
+
+            var distance = countDownDate - now;
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+ document.getElementById("countdown").innerHTML = days + "d : " + hours + "h : " + minutes + "m : " +seconds + "s";
+
+                
+                
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = '<a href="" id="download">Download!</a>';
+            }
+        
+     }, 1000);
+
+
